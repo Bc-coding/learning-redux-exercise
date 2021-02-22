65b2c9056c1206a6cd24ca25aefc8fed44dbb48b
Belle Carrie <bc.coding@hotmail.com>
2021-02-22 20:19:34 +0000

    refactor decrease reducer action

--- src/reducer.js
+++ src/reducer.js
@@ -23,23 +23,34 @@ function reducer(state, action) {
   }
 
   if (action.type === DECREASE) {
-    let tempCart = [];
+    // let tempCart = [];
 
-    if (action.payload.amount === 1) {
-      tempCart = state.cart.filter(
-        cartItem => cartItem.id !== action.payload.id
-      );
-    } else {
-      tempCart = state.cart.map(cartItem => {
-        if (cartItem.id === action.payload.id) {
-          cartItem = {
-            ...cartItem,
-            amount: cartItem.amount - 1,
-          };
-        }
-        return cartItem;
-      });
-    }
+    // if (action.payload.amount === 1) {
+    //   tempCart = state.cart.filter(
+    //     cartItem => cartItem.id !== action.payload.id
+    //   );
+    // } else {
+    //   tempCart = state.cart.map(cartItem => {
+    //     if (cartItem.id === action.payload.id) {
+    //       cartItem = {
+    //         ...cartItem,
+    //         amount: cartItem.amount - 1,
+    //       };
+    //     }
+    //     return cartItem;
+    //   });
+    // }
+
+    // refactoring the code
+    let tempCart = state.cart.map(cartItem => {
+      if (cartItem.id === action.payload.id) {
+        cartItem = {
+          ...cartItem,
+          amount: cartItem.amount - 1,
+        };
+      }
+      return cartItem;
+    });
 
     return { ...state, cart: tempCart };
   }
